97c11c8b9d16e44a9dc8487ad674ebe035f7c9e5
bscitdrive <bscitdrive2023@gmail.com>
2025-11-25 22:23:49 +0530

    Update Prac2_5_xPath.java

--- prac2/Prac2_5_xPath.java
+++ prac2/Prac2_5_xPath.java
@@ -1,4 +1,7 @@
-
+/*
+Write a script to locate elements on https://demo.guru99.com/test/selenium-xpath.html 
+using xpath.
+*/
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -29,3 +32,4 @@ public class Prac2_5_xPath {
 	}
 
 }
+
